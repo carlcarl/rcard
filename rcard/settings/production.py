4003fe7dba1d1e5c225361a1475b627993b65b3e
carlcarl <carlcarlking@gmail.com>
2015-07-31 11:12:38 +0800

    Add DEBUG=False to production setting

--- rcard/settings/production.py
+++ rcard/settings/production.py
@@ -1,4 +1,6 @@
 from rcard.settings.staging import *
 
+DEBUG = False
+
 # Uncomment if using celery worker configuration
 # BROKER_URL = 'amqp://karma2_production:%(BROKER_PASSWORD)s@%(BROKER_HOST)s/karma2_production' % os.environ
