e745cbd16cd2eef2a5805aa7bd113bcaf147af4e
carlcarl <carlcarlking@gmail.com>
2015-08-09 17:14:17 +0800

    Correct the url format of image API

--- waterfall_wall/serializers.py
+++ waterfall_wall/serializers.py
@@ -4,6 +4,11 @@ from rest_framework import serializers
 
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.SerializerMethodField()
+
+    def get_url(self, obj):
+        return obj.path.url
+
     class Meta:
         model = Image
-        fields = ('path', 'url', 'nude_percent')
+        fields = ('url', 'nude_percent')
