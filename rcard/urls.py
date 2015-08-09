4cd04558d788e9c1ee77471e87f8ef27416bf60e
carlcarl <carlcarlking@gmail.com>
2015-08-09 17:32:38 +0800

    Add api prefix for image API

--- rcard/urls.py
+++ rcard/urls.py
@@ -23,7 +23,7 @@ router = routers.DefaultRouter()
 router.register(r'images', views.ImageViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
